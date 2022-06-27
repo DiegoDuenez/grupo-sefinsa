e9d0af0f2230e59567929d538c498109ba418eb8
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-06-27 11:26:39 -0500

    delete: Se quito codigo comentado

--- php/Clientes/App.php
+++ php/Clientes/App.php
@@ -69,7 +69,6 @@ switch($func){
         $ruta_id = $_POST['ruta_id'];
         $poblacion_id = $_POST['poblacion_id'];
         $garantias_cliente = $_POST['garantias_cliente'];
-
         
         $nombre_aval = $_POST['nombre_aval'];
         $direccion_aval = $_POST['direccion_aval'];
@@ -99,62 +98,10 @@ switch($func){
         FileManager::moveTo(FileManager::get('archivo_aval_1','tmp_name'), $ruta_archivos_aval.FileManager::get('archivo_aval_1','name'));
 
 
-        /*$archivos_cliente = "['$ruta_archivos_cliente". FileManager::get('archivo_cliente_0','name') . "', ".
-        "'$ruta_archivos_cliente".FileManager::get('archivo_cliente_1','name') . "', ".
-        "'$ruta_archivos_cliente".FileManager::get('archivo_cliente_2','name') . "', ".
-        "'$ruta_archivos_cliente".FileManager::get('archivo_cliente_3','name') . "', ".
-        "'$ruta_archivos_cliente".FileManager::get('archivo_cliente_4','name') . "', ".
-        "]";
-
-        $archivos_aval = "['$ruta_archivos_aval". FileManager::get('archivo_aval_0','name') . "', ".
-        "'$ruta_archivos_aval".FileManager::get('archivo_aval_1','name') . "', ".
-        "]";*/
-
-        $archivos_cliente = "";
-        $archivos_aval = "";
-
         echo $Cliente->create($nombre_cliente, $direccion_cliente, $telefono_cliente, $or_cliente, $nueva_carpeta_cliente,
         $nombre_aval, $direccion_aval, $telefono_aval, $or_aval, $nueva_carpeta_aval, $colocadora_id, $garantias_cliente, 
         $garantias_aval, $ruta_id, $poblacion_id, $nueva_carpeta_cliente, $nueva_carpeta_aval);
 
-        //FileManager::createFolder('../../resources/comprobantes/diego');
-
-        //FileManager::renameFolder('../../resources/comprobantes/diego/', '../../resources/comprobantes/diego03');
-
-
-       /* if ( 0 < FileManager::errors('domicilio_cliente') || 0 < FileManager::errors('ine_cliente')  ) {
-            echo FileManager::errors('domicilio_cliente');
-            die();
-        }
-        else {
-
-            //FileManager::createFolder('../../resources/comprobantes/diego');
-
-            $c_domicilio_cliente =  'resources/comprobantes/'.FileManager::get('domicilio_cliente','name');
-            $c_ine_cliente =  'resources/comprobantes/'.FileManager::get('ine_cliente','name');
-            $c_tarjeton_cliente =  'resources/comprobantes/'.FileManager::get('tarjeton_cliente','name');
-            $c_contrato_cliente =  'resources/comprobantes/'.FileManager::get('contrato_cliente','name');
-            $c_pagare_cliente =  'resources/comprobantes/'.FileManager::get('pagare_cliente','name');
-
-            $c_domicilio_aval =  'resources/comprobantes/'.FileManager::get('domicilio_aval','name');
-            $c_ine_aval =  'resources/comprobantes/'.FileManager::get('ine_aval','name');
-
-            FileManager::moveTo(FileManager::get('domicilio_cliente','tmp_name'), '../../'.$c_domicilio_cliente);
-            FileManager::moveTo(FileManager::get('ine_cliente','tmp_name'), '../../'.$c_ine_cliente);
-            FileManager::moveTo(FileManager::get('tarjeton_cliente','tmp_name'), '../../'.$c_tarjeton_cliente);
-            FileManager::moveTo(FileManager::get('contrato_cliente','tmp_name'), '../../'.$c_contrato_cliente);
-            FileManager::moveTo(FileManager::get('pagare_cliente','tmp_name'), '../../'.$c_pagare_cliente);
-
-            FileManager::moveTo(FileManager::get('domicilio_aval','tmp_name'), '../../'.$c_domicilio_aval);
-            FileManager::moveTo(FileManager::get('ine_aval','tmp_name'), '../../'.$c_ine_aval);
-
-            echo $Cliente->create($nombre_cliente, $direccion_cliente, $telefono_cliente, $or_cliente, $c_domicilio_cliente, $c_ine_cliente, $c_tarjeton_cliente, $c_contrato_cliente, $c_pagare_cliente,
-                            $nombre_aval, $direccion_aval, $telefono_aval, $or_aval, $c_domicilio_aval, $c_ine_aval);
-
-
-        }*/
-
-        
         
     break;
 
