2fdfabfb2015cf07ea5fbf7d64427ab732ba9b41
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-06-30 15:58:48 -0500

    change: env example

--- php/env.example.php
+++ php/env.example.php
@@ -7,9 +7,4 @@ return [
     'user'      => 'root',
     'password'  => '',
     'database'  => ''
-    /*'driver' => 'mysql',
-    'host' => 'awstorreoncom2.ipowermysql.com',
-    'user' => 'ufinanzas',
-    'password' => 'AWsw2022$$',
-    'database' => 'bd_finanzas'*/
 ];
